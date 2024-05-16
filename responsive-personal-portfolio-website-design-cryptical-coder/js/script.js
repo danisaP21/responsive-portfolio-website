4c2bdc6fa80afcdab93bcf164423e317cef73341
Danisa Herviani <dnsviani@gmail.com>
2024-05-16 19:48:00 +0700

    text-typing-animation done

---
+++ responsive-personal-portfolio-website-design-cryptical-coder/js/script.js
@@ -0,0 +1,7 @@
+/* ===== Typing Animation ===== */
+var typed = new Typed(".typing", {
+  strings: ["", "Web Designer", "Web Developer", "Graphic Designer"],
+  typerSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
